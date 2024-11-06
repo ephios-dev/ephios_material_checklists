929e9e58b6aab752aabd7c1563c1a9c579adee79
Christian Zöllner <christian.zoellner@hpi.de>
2024-11-06 18:40:28 +0100

    add expiry date property to itemtype model and reformat files

--- ephios_material_checklists/models/itemtype.py
+++ ephios_material_checklists/models/itemtype.py
@@ -4,28 +4,42 @@ from django.utils.translation import gettext as _
 
 class ItemTypeCategory(models.Model):
     name = models.CharField(max_length=254, unique=True, verbose_name=_("name"))
-    order_key = models.IntegerField(blank=False, default=1, verbose_name=_("ordering index"))
+    order_key = models.IntegerField(
+        blank=False, default=1, verbose_name=_("ordering index")
+    )
 
     class Meta:
         verbose_name = _("item type category")
         verbose_name_plural = _("item type categories")
-        ordering = ("order_key", "name",)
+        ordering = (
+            "order_key",
+            "name",
+        )
 
     def __str__(self):
         return self.name
 
 
 class ItemType(models.Model):
-    name = models.CharField(max_length=254, unique=True, blank=False, verbose_name=_("name"))
-    # TODO: Name with additional HTML formating
-    category = models.ForeignKey(ItemTypeCategory, null=False, blank=False, on_delete=models.CASCADE,
-                                 related_name="items", verbose_name = _("category"))
-    notes = models.TextField(_("notes"), blank=True)
+    name = models.CharField(
+        max_length=254, unique=True, blank=False, verbose_name=_("name")
+    )
+    # TODO: Add additional name field with extra HTML formating
+    category = models.ForeignKey(
+        ItemTypeCategory,
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE,
+        related_name="items",
+        verbose_name=_("category"),
+    )
+    has_expiry_date = models.BooleanField(default=True, verbose_name="has expiry date")
+    notes = models.TextField(blank=True, verbose_name=_("notes"))
     deprecated = models.BooleanField(default=False, verbose_name=_("deprecated"))
 
-    # TODO: Images / Thumbnails
+    # TODO: Add field for image / thumbnail
 
-    # TODO: Packaging Sizes
+    # TODO: Add field for packaging size(s)
 
     def save(self, *args, **kwargs):
         return super().save(*args, **kwargs)
@@ -33,7 +47,13 @@ class ItemType(models.Model):
     class Meta:
         verbose_name = _("item type")
         verbose_name_plural = _("item types")
-        ordering = ("category__order_key", "category__name", "name",)
+        ordering = (
+            "category__order_key",
+            "category__name",
+            "name",
+        )
 
     def __str__(self):
-        return  f"{self.name} " + f" ({_('deprecated')})" if self.deprecated else self.name
+        return (
+            f"{self.name} " + f" ({_('deprecated')})" if self.deprecated else self.name
+        )
--- ephios_material_checklists/tests/test_itemtype.py
+++ ephios_material_checklists/tests/test_itemtype.py
@@ -4,7 +4,7 @@ from django.test import TestCase
 from ephios_material_checklists.models.itemtype import ItemTypeCategory, ItemType
 
 
-class MaterialItemGroupTypeTestCase(TestCase):
+class ItemTypeCategoryTestCase(TestCase):
 
     # Attribute 'name'
 
@@ -12,6 +12,9 @@ class MaterialItemGroupTypeTestCase(TestCase):
         with self.assertRaises(Exception):
             category = ItemTypeCategory.objects.create(order_key=1)
             category.full_clean()
+            self.fail(
+                "It should not be possible to create an item type category without a name."
+            )
 
     def test_item_type_category_name_is_unique(self):
         this_category = ItemTypeCategory.objects.create(name="unique")
@@ -19,6 +22,9 @@ class MaterialItemGroupTypeTestCase(TestCase):
         with self.assertRaises(Exception):
             other_category = ItemTypeCategory.objects.create(name="unique")
             other_category.full_clean()
+            self.fail(
+                "It should not be possible to create an item type category with a name that is not unique."
+            )
 
     # Attribute 'order key'
 
@@ -29,19 +35,27 @@ class MaterialItemGroupTypeTestCase(TestCase):
             category_2 = ItemTypeCategory.objects.create(name="y")
             category_2.full_clean()
         except Exception:
-            self.fail("It should be possible to create an item type category without 'order key' attribute.")
-        self.assertEqual(category_1.order_key, category_2.order_key,
-                         "New item type categorys should have similar order key by default.")
+            self.fail(
+                "It should be possible to create an item type category without 'order key' attribute."
+            )
+        self.assertEqual(
+            category_1.order_key,
+            category_2.order_key,
+            "New item type categories should have similar order key by default.",
+        )
 
     # String representation
 
     def test_item_type_category_string_representation(self):
         this_category = ItemTypeCategory.objects.create(name="Group")
-        self.assertIn("Group", str(this_category), "Group name should be in item type category string representation.")
+        self.assertIn(
+            "Group",
+            str(this_category),
+            "Group name should be in item type category string representation.",
+        )
 
 
-
-class MaterialItemTypeTestCase(TestCase):
+class ItemTypeTestCase(TestCase):
 
     def setUp(self):
         self.category = ItemTypeCategory.objects.create(name="category")
@@ -52,20 +66,37 @@ class MaterialItemTypeTestCase(TestCase):
         with self.assertRaises(Exception):
             item = ItemType.objects.create(category=self.category)
             item.full_clean()
+            self.fail("It should not be possible to create an item type without name.")
 
     def test_item_type_name_is_not_empty(self):
         with self.assertRaises(Exception):
             item = ItemType.objects.create(name="", category=self.category)
             item.full_clean()
+            self.fail(
+                "It should not be possible to create an item type with empty name."
+            )
 
     def test_item_type_name_is_unique(self):
         with self.assertRaises(Exception):
             category_1 = ItemTypeCategory.objects.create(name="A")
-            item_1 = ItemType.objects.create(name="duplicate", category=category_1)
+            item_1 = ItemType.objects.create(
+                name="duplicate",
+                category=category_1,
+                has_expiry_date=False,
+                deprecated=False,
+            )
             item_1.full_clean()
             category_2 = ItemTypeCategory.objects.create(name="B")
-            item_2 = ItemType.objects.create(name="duplicate", category=category_2)
+            item_2 = ItemType.objects.create(
+                name="duplicate",
+                category=category_2,
+                has_expiry_date=True,
+                deprecated=False,
+            )
             item_2.full_clean()
+            self.fail(
+                "It should not be possible to create an item type when one already exists with the same name."
+            )
 
     # Attribute 'category'
 
@@ -73,30 +104,85 @@ class MaterialItemTypeTestCase(TestCase):
         with self.assertRaises(Exception):
             item = ItemType.objects.create(name="xxx")
             item.full_clean()
+            self.fail(
+                "It should not be possible to create an item type without a category."
+            )
+
+    # Attribute 'expiry date'
+
+    def test_item_type_expiry_date_is_true_by_default(self):
+        try:
+            item = ItemType.objects.create(
+                name="name", category=self.category, deprecated=True
+            )
+            item.full_clean()
+        except Exception:
+            self.fail(
+                "It should be possible to create an item type without 'has_expiry_date' attribute."
+            )
+        self.assertTrue(
+            item.deprecated, "New item type should have expiry date by default."
+        )
 
     # Attribute 'notes'
 
-    def test_item_type_notes_are_empty_by_default(self):
+    def test_item_type_can_be_created_without_notes_attribute(self):
         try:
-            item = ItemType.objects.create(name="name", category=self.category)
+            item = ItemType.objects.create(
+                name="name",
+                category=self.category,
+                has_expiry_date=True,
+                deprecated=True,
+            )
             item.full_clean()
         except Exception:
-            self.fail("It should be possible to create an item type without 'notes' attribute.")
-        self.assertEquals("", item.notes, "New item type note should be empty text by default.")
+            self.fail(
+                "It should be possible to create an item type without 'notes' attribute."
+            )
+
+    def test_item_type_notes_are_empty_by_default(self):
+        item = ItemType.objects.create(
+            name="name",
+            category=self.category,
+            has_expiry_date=True,
+            deprecated=True,
+        )
+        self.assertEquals(
+            "", item.notes, "New item type note should be empty text by default."
+        )
 
     # Attribute 'deprecated'
 
-    def test_item_type_deprecated_is_false_by_default(self):
+    def test_item_type_can_be_created_without_deprecation_attribute(self):
         try:
-            item = ItemType.objects.create(name="name", category=self.category)
+            item = ItemType.objects.create(
+                name="name", category=self.category, has_expiry_date=True
+            )
             item.full_clean()
         except Exception:
-            self.fail("It should be possible to create an item type without 'deprecated' attribute.")
-        self.assertFalse(item.deprecated, "New item type should not be deprecated by default.")
+            self.fail(
+                "It should be possible to create an item type without 'deprecated' attribute."
+            )
+
+    def test_item_type_deprecated_is_false_by_default(self):
+        item = ItemType.objects.create(
+            name="name", category=self.category, has_expiry_date=True
+        )
+        self.assertFalse(
+            item.deprecated, "New item type should not be deprecated by default."
+        )
 
     # String representation 'notes'
 
     def test_item_type_string_representation(self):
         item = ItemType.objects.create(name="name", category=self.category)
-        self.assertIn("name", str(item), "Item Type unformatted name should be in item type string repr.")
-        self.assertNotIn("category", str(item), "Group name should NOT be in item type string representation.")
+        self.assertIn(
+            "name",
+            str(item),
+            "Item Type name should be in item type string representation.",
+        )
+        self.assertNotIn(
+            "category",
+            str(item),
+            "Category name should NOT be in item type string representation.",
+        )
